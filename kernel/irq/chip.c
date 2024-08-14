5e16e36442b11c24fb2ae54dfaa5a571d0685be2
Santhosh <santhosh.user.why.red@gmail.com>
2024-08-14 21:11:02 +0530

    drivers: irq: Fix unbalanced IRQ enabling
    The irq_enable was updated to ensure proper IRQ state management.
    
    Previously, IRQs could be enabled even if they were already in the enabled state or not correctly handled, leading to warnings and potential state inconsistencies.
    
    [ 1761.420024] WARNING: CPU: 5 PID: 19323 at irq_startup+0x16c/0x310                                                  [ 1761.420036] CPU: 5 PID: 19323 Comm: bluetooth@1.0-s Tainted: G        W         4.19.318-San-Kernel-BumbleBee-R1.1>
    [ 1761.420039] Hardware name: Qualcomm Technologies, Inc. SDM 636 PM660 + PM660L MTP, Whyred (DT)                     [ 1761.420043] pstate: 60000085 (nZCv daIf -PAN -UAO)
    [ 1761.420047] pc : irq_startup+0x16c/0x310                                                                           [ 1761.420052] lr : enable_irq+0x7c/0x100
    [ 1761.420053] sp : fffffff2d2b077e0
    [ 1761.420055] x29: fffffff2d2b077e0 x28: 0000000000000001 x27: 0000000000000000
    [ 1761.420060] x26: fffffff2d2b07948 x25: fffffff2382b5c00 x24: 0000000000000000
    [ 1761.420064] x23: ffffff9c6ac06000 x22: ffffff9c69a60180 x21: fffffff2f847da18                                      [ 1761.420068] x20: 0000000000000001 x19: fffffff2f847da00 x18: 000000000007ffff
    [ 1761.420072] x17: 0000000040000000 x16: 0000000080000000 x15: 0000000000000000
    [ 1761.420075] x14: ffffff9c6ae4cad0 x13: 0000000000000001 x12: 0000000000000001
    [ 1761.420078] x11: 0000000000000001 x10: 0000000000000000 x9 : 0000000003030004
    [ 1761.420082] x8 : fffffff2f847da00 x7 : 0000000000000000 x6 : 0000000000000000
    [ 1761.420085] x5 : 0000000000000000 x4 : 0000000000000001 x3 : 0000000000000001
    [ 1761.420088] x2 : 0000000000000001 x1 : 0000000000000001 x0 : fffffff2f847da00
    [ 1761.420093]
                   SP: -56594761888:
    [ 1761.420095] 7760  00000000 00000000 382b5c00 fffffff2 d2b07948 fffffff2 00000000 00000000
    [ 1761.420103] 7780  00000001 00000000 d2b077e0 fffffff2 6932c66c ffffff9c d2b077e0 fffffff2
    [ 1761.420110] 77a0  6933076c ffffff9c 60000085 00000000 382b5c00 fffffff2 693deee8 ffffff9c
    [ 1761.420117] 77c0  ffffffff 0000007f 6ac06000 ffffff9c d2b077e0 fffffff2 6933076c ffffff9c
    [ 1761.420124] 77e0  d2b07820 fffffff2 6932c66c ffffff9c f7747048 fffffff2 000000c0 00000000
    [ 1761.420131] 7800  f8482c10 fffffff2 f847da00 fffffff2 00000000 00000000 78c51e00 8b4b8267
    [ 1761.420137] 7820  d2b07850 fffffff2 699914a4 ffffff9c f8482c10 fffffff2 f7747040 fffffff2
    [ 1761.420144] 7840  d2b07958 fffffff2 00000000 00000000 d2b07870 fffffff2 69992624 ffffff9c
    [ 1761.420152] Call trace:
    [ 1761.420156]  irq_startup+0x16c/0x310
    [ 1761.420158]  enable_irq+0x7c/0x100
    [ 1761.420165]  disable_wakeup_interrupt+0x44/0x120
    [ 1761.420168]  msm_hs_pm_resume+0x84/0x1b0
    [ 1761.420173]  msm_hs_runtime_resume+0xc/0x20
    [ 1761.420179]  pm_generic_runtime_resume+0x24/0x40
    [ 1761.420182]  __rpm_callback+0x1c0/0x410
    [ 1761.420186]  rpm_resume+0x538/0x940
    [ 1761.420189]  __pm_runtime_resume+0x74/0xf0
    [ 1761.420192]  msm_hs_startup+0x9c/0x890
    [ 1761.420195]  uart_startup+0x1c8/0x340
    [ 1761.420199]  uart_port_activate+0x48/0x90
    [ 1761.420201]  tty_port_open+0xdc/0x160
    [ 1761.420204]  uart_open+0x1c/0x30
    [ 1761.420209]  tty_open+0x398/0x6e0
    [ 1761.420214]  chrdev_open+0xe4/0x2b0
    [ 1761.420219]  do_dentry_open+0x180/0x340
    [ 1761.420222]  vfs_open+0x28/0x30
    [ 1761.420225]  path_openat+0xae8/0xdf0
    [ 1761.420227]  do_filp_open+0xa8/0x160
    [ 1761.420231]  do_sys_open+0x130/0x240
    [ 1761.420234]  __arm64_sys_openat+0x24/0x30
    [ 1761.420239]  el0_svc_common+0xb0/0x120
    [ 1761.420241]  el0_svc_handler+0x20/0x80
    [ 1761.420245]  el0_svc+0x8/0x240
    [ 1761.420247] ---[ end trace b26279619e0536b7 ]---
    
    Signed-off-by: Santhosh <santhosh.user.why.red@gmail.com>

--- kernel/irq/chip.c
+++ kernel/irq/chip.c
@@ -305,9 +305,7 @@ void irq_shutdown(struct irq_desc *desc)
 
 void irq_enable(struct irq_desc *desc)
 {
-	if (!irqd_irq_disabled(&desc->irq_data)) {
-		unmask_irq(desc);
-	} else {
+	if (irqd_irq_disabled(&desc->irq_data)) {
 		irq_state_clr_disabled(desc);
 		if (desc->irq_data.chip->irq_enable) {
 			desc->irq_data.chip->irq_enable(&desc->irq_data);
@@ -315,6 +313,8 @@ void irq_enable(struct irq_desc *desc)
 		} else {
 			unmask_irq(desc);
 		}
+	} else {
+		WARN(1, "IRQ %d is already enabled\n", desc->irq_data.irq);
 	}
 }
 
