b3b1157d94b0ef2157aee4dfd35bbc96bd604ae4
rsuntk <rissu.ntk@gmail.com>
2025-03-12 16:56:57 +0000

    arch: Import OEM Changes
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- arch/arm64/kernel/cpufeature.c
+++ arch/arm64/kernel/cpufeature.c
@@ -1137,6 +1137,7 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.min_field_value = 1,
 	},
 #endif
+#ifndef CONFIG_MTK_IGNORE_SSBS
 #ifdef CONFIG_ARM64_SSBD
 	{
 		.desc = "Speculative Store Bypassing Safe (SSBS)",
@@ -1149,6 +1150,7 @@ static const struct arm64_cpu_capabilities arm64_features[] = {
 		.min_field_value = ID_AA64PFR1_SSBS_PSTATE_ONLY,
 		.cpu_enable = cpu_enable_ssbs,
 	},
+#endif
 #endif
 	{},
 };
