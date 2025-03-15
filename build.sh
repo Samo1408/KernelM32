398fe804455ed5b1faffc0665c6f5e35b0767a3f
Rissu <90097027+rsuntk@users.noreply.github.com>
2025-03-15 11:04:43 +0700

    Build: Use susfs branch

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@
 [ -z $DO_CLEAN ] && DO_CLEAN=false
 [ -z $LTO ] && LTO=none
 [ -z $DEFAULT_KSU_REPO ] && DEFAULT_KSU_REPO="https://raw.githubusercontent.com/rsuntk/KernelSU/main/kernel/setup.sh"
-[ -z $DEFAULT_KSU_BRANCH ] && DEFAULT_KSU_BRANCH="main"
+[ -z $DEFAULT_KSU_BRANCH ] && DEFAULT_KSU_BRANCH="susfs-v1.5.5"
 [ -z $DEFAULT_AK3_REPO ] && DEFAULT_AK3_REPO="https://github.com/rsuntk/AnyKernel3.git"
 [ -z $DEVICE ] && DEVICE="M325FV"
 [ -z $IMAGE ] && IMAGE="$(pwd)/out/arch/arm64/boot/Image"
