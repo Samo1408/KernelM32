3d1e65ffb4686a626a979239cbae0680bfa079e4
rsuntk <rissu.ntk@gmail.com>
2025-06-05 06:40:34 +0000

    android: fix duplicated variable for BINDER_DEBUG_USER_ERROR
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- drivers/android/binder_alloc.c
+++ drivers/android/binder_alloc.c
@@ -44,7 +44,6 @@ enum {
 	BINDER_DEBUG_OPEN_CLOSE             = 1U << 1,
 	BINDER_DEBUG_BUFFER_ALLOC           = 1U << 2,
 	BINDER_DEBUG_BUFFER_ALLOC_ASYNC     = 1U << 3,
-	BINDER_DEBUG_USER_ERROR             = 1U << 4,
 };
 static uint32_t binder_alloc_debug_mask = BINDER_DEBUG_USER_ERROR;
 
