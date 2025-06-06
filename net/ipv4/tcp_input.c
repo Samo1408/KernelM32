657a134f8a9273aa28e999987abbe9e4edf93cee
rsuntk <rissu.ntk@gmail.com>
2025-06-06 17:50:28 +0000

    Revert "UPSTREAM: net: Record receive queue number for a connection"
    This reverts commit dabaeae8f627911e808cee400fd365dec558c2ed.
    
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -73,7 +73,6 @@
 #include <net/dst.h>
 #include <net/tcp.h>
 #include <net/inet_common.h>
-#include <net/busy_poll.h>
 #include <linux/ipsec.h>
 #include <asm/unaligned.h>
 #include <linux/errqueue.h>
@@ -5676,7 +5675,6 @@ void tcp_finish_connect(struct sock *sk, struct sk_buff *skb)
 	if (skb) {
 		icsk->icsk_af_ops->sk_rx_dst_set(sk, skb);
 		security_inet_conn_established(sk, skb);
-		sk_mark_napi_id(sk, skb);
 	}
 
 	/* Make sure socket is routed, for correct metrics.  */
@@ -6508,7 +6506,6 @@ int tcp_conn_request(struct request_sock_ops *rsk_ops,
 	tcp_rsk(req)->snt_isn = isn;
 	tcp_rsk(req)->txhash = net_tx_rndhash();
 	tcp_openreq_init_rwin(req, sk, dst);
-	sk_rx_queue_set(req_to_sk(req), skb);
 	if (!want_cookie) {
 		tcp_reqsk_record_syn(sk, req, skb);
 		fastopen_sk = tcp_try_fastopen(sk, skb, req, &foc);
