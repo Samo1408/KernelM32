1d9a57d94d117cb416e5401cb51d242cfdc39d60
Samo141988 <154265838+Samo141988@users.noreply.github.com>
2025-07-24 14:49:07 +0300

    Update buildsusfs.sh

--- buildsusfs.sh
+++ buildsusfs.sh
@@ -8,7 +8,7 @@
 # << If unset, you can override if u want
 [ -z $IS_CI ] && IS_CI=true
 [ -z $DO_CLEAN ] && DO_CLEAN=false
-[ -z $LTO ] && LTO=full
+[ -z $LTO ] && LTO=none
 [ -z $DEFAULT_KSU_REPO ] && DEFAULT_KSU_REPO="https://raw.githubusercontent.com/Samo141988/KernelSU-Next/next-susfs-new/kernel/setup.sh"
 [ -z $DEFAULT_KSU_BRANCH ] && DEFAULT_KSU_BRANCH="next-susfs-new"
 [ -z $DEFAULT_AK3_REPO ] && DEFAULT_AK3_REPO="https://github.com/Samo141988/AnyKernel3.git"
