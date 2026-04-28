ac730d0fca961e41ae7154f72cc5d256cfb74b4e
Samo1408 <ab.0ss0mm@gmail.com>
2026-04-28 21:44:36 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,9 +8,9 @@
 # << If unset, you can override if u want
 [ -z $IS_CI ] && IS_CI=false
 [ -z $DO_CLEAN ] && DO_CLEAN=false
-[ -z $LTO ] && LTO=none
-[ -z $DEFAULT_KSU_REPO ] && DEFAULT_KSU_REPO="https://raw.githubusercontent.com/Samo141988/KernelSU-Next/next-susfs-new/kernel/setup.sh"
-[ -z $DEFAULT_KSU_BRANCH ] && DEFAULT_KSU_BRANCH="next-susfs-new"
+[ -z $LTO ] && LTO=full
+[ -z $DEFAULT_KSU_REPO ] && DEFAULT_KSU_REPO="https://raw.githubusercontent.com/Samo1408/ReSukiSU/susfs-ksud/kernel/setup.sh"
+[ -z $DEFAULT_KSU_BRANCH ] && DEFAULT_KSU_BRANCH="susfs-ksud"
 [ -z $DEFAULT_AK3_REPO ] && DEFAULT_AK3_REPO="https://github.com/Samo141988/AnyKernel3.git"
 [ -z $DEVICE ] && DEVICE="M325FV"
 [ -z $IMAGE ] && IMAGE="$(pwd)/out/arch/arm64/boot/Image"
@@ -114,7 +114,7 @@ pr_post_build() {
 	[ "$@" = "failed" ] && echo -e "${R}#### Failed to build some targets ($BUILD_TARGET) ####${N}" ||	echo -e "${G}#### Build completed at `date` ####${N}"
 	echo ""
 	echo "======================================================="
-	[ -e $IMAGE ] && strings $IMAGE | grep "Linux version" || exit
+	[ -e $IMAGE ] && strings $IMAGE | grep "Linux Version" || exit
 	echo "======================================================="
 }
 
